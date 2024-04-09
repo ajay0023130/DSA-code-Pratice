5e58edbfeb98a5195ad0e8aa68d032c7af98c28c
ajay0023130 <ajaybabu0046@gmail.com>
2024-04-09 14:14:41 +0530

    first_class

---
+++ oops/1_chapter.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self,brand,model):
+        self.brand= brand
+        self.model =model
+
+my_car = Car('Toyota','corola')
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car = Car('Tata','safari')
+print(my_new_car.brand)
+print(my_new_car.model)
